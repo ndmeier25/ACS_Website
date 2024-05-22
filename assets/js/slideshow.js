596306c432bbac802d6d942bc818fb33bf8126ec
ndmeier25 <ndmeier@umich.edu>
2024-05-22 19:00:31 -0400

    Added Slideshows, extra social links

---
+++ assets/js/slideshow.js
@@ -0,0 +1,28 @@
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
+}
+
+function currentSlide(n, no) {
+    showSlides(slideIndex[no] = n, no);
+}
+
+function showSlides(n, no) {
+    let i;
+    let x = document.getElementsByClassName(slideId[no]);
+    let dots = document.getElementsByClassName("dot");
+    if (n > x.length) {slideIndex[no] = 1}    
+    if (n < 1) {slideIndex[no] = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    x[slideIndex[no]-1].style.display = "block"; 
+    dots[slideIndex[no]-1].className += " active";
+}
